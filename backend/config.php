08fdb7cb7a56728d18ad1af21f449770df7acb16
grigorka95 <trane7775@gmail.com>
2025-09-11 13:23:09 +0000

    дубль кода утранен

--- backend/config.php
+++ backend/config.php
@@ -8,10 +8,3 @@ return [
     'site_origin' => 'https://example.com', // для CORS / безопасности, замените на ваш домен (или '*')
     'max_reviews_per_ip_per_day' => 5,
 ];
-
-return [
-    'db_path' => __DIR__ . '/../data/reviews.sqlite',
-    'admin_email' => 'dr.fatenko@example.com', // поставьте нужный email
-    'site_origin' => 'https://localhost:8000', // для CORS / безопасности, замените на ваш домен (или '*')
-    'max_reviews_per_ip_per_day' => 5,
-];
