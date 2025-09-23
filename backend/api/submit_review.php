48957dfc78bc4fedb0329eba8392671331eb2b58
grigorka95 <trane7775@gmail.com>
2025-09-23 17:29:33 +0300

    Update submit_review.php
    промежуточный комит переделка сабмит ревью

--- backend/api/submit_review.php
+++ backend/api/submit_review.php
@@ -7,10 +7,10 @@ $config = require __DIR__ . '/../config.php';
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     header('Access-Control-Allow-Methods: POST, OPTIONS');
     header('Access-Control-Allow-Headers: Content-Type');
-    header('Access-Control-Allow-Origin: ' . $config['site_origin' => '*']);
+    header('Access-Control-Allow-Origin: ' . ($config['site_origin'] ?? '*'));
     exit;
 }
-header('Access-Control-Allow-Origin: ' . $config['site_origin' => '*']);
+header('Access-Control-Allow-Origin: ' . ($config['site_origin'] ?? '*'));
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     json_response(['error' => 'Method not allowed'], 405);
