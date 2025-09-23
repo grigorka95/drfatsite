9e354ba3966e6a919385ae0c4e31a7a1ac69f5c2
grigorka95 <trane7775@gmail.com>
2025-09-23 15:06:51 +0300

    Update get_token.php
    ошибка, проверка статуса сессии и создание сессии

--- backend/api/get_token.php
+++ backend/api/get_token.php
@@ -1,4 +1,5 @@
 <?php
 require __DIR__ . '/../functions.php';
-header('Content-Type: application/json');
+header('Content-Type: application/json; charset=utf-8');
+if (session_status() === PHP_SESSION_NONE) session_start();
 echo json_encode(['token' => getCsrfToken()]);
