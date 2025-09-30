ef3363713bad642b1e27df45d028942efbc32432
grigorka95 <trane7775@gmail.com>
2025-09-30 10:01:13 +0300

    Update get_token.php
    запуск сессии только в function.php

--- backend/api/get_token.php
+++ backend/api/get_token.php
@@ -1,5 +1,4 @@
 <?php
 require __DIR__ . '/../functions.php';
 header('Content-Type: application/json; charset=utf-8');
-if (session_status() === PHP_SESSION_NONE) session_start();
 echo json_encode(['token' => getCsrfToken()]);
