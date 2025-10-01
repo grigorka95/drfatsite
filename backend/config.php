3c890a5116b5a513e47896cad3107f8f90b0e68c
grigorka95 <trane7775@gmail.com>
2025-10-01 16:04:05 +0300

    Update config.php
    с доменом

--- backend/config.php
+++ backend/config.php
@@ -8,6 +8,6 @@ return [
     'db_user' => 'if0_40057416',
     'db_pass' => 'm6z7qbmGpQ2b',
     'admin_email' => 'dr.fatenko@example.com', // поставьте нужный email
-    'site_origin' => '*', // для CORS / безопасности, замените на ваш домен (или '*')
+    'site_origin' => 'drfatenko.great-site.net', // для CORS / безопасности, замените на ваш домен (или '*')
     'max_reviews_per_ip_per_day' => 5,
 ];
