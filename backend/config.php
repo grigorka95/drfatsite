9074f39e61078f249d367549c533d79f3fce8008
grigorka95 <trane7775@gmail.com>
2025-10-01 16:01:14 +0300

    Update config.php
    Обновляем конфиг для перехода в mysql

--- backend/config.php
+++ backend/config.php
@@ -3,7 +3,10 @@
 // backend/config.php
 
 return [
-    'db_path' => __DIR__ . '/../data/reviews.sqlite',
+    'db_host' => 'sql110.infinityfree.com',
+    'db_name' => 'if0_40057416_reviews',
+    'db_user' => 'if0_40057416',
+    'db_pass' => 'm6z7qbmGpQ2b',
     'admin_email' => 'dr.fatenko@example.com', // поставьте нужный email
     'site_origin' => '*', // для CORS / безопасности, замените на ваш домен (или '*')
     'max_reviews_per_ip_per_day' => 5,
