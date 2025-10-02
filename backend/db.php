95c07fa0d54dbe94b24a81c968e961142eb5a207
grigorka95 <trane7775@gmail.com>
2025-10-02 11:54:57 +0300

    Update db.php
    лишние пробелы в pdo в запросах

--- backend/db.php
+++ backend/db.php
@@ -6,7 +6,7 @@ function getPDO() {
     static $pdo = null;
     if ($pdo === null) {
         $cfg = require __DIR__ . '/config.php';
-        $dsn = "mysql:host={$cfg['db_host']};dbname = {$cfg['db_name']};charset=utf8mb4";
+        $dsn = "mysql:host={$cfg['db_host']};dbname={$cfg['db_name']};charset=utf8mb4";
         $pdo = new PDO($dsn, $cfg['db_user'], $cfg['db_pass']);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
