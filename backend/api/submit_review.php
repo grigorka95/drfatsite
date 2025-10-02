3f57f295a2cd0bba890a08036ef8d719fad50cca
grigorka95 <trane7775@gmail.com>
2025-10-02 13:01:41 +0300

    Update submit_review.php
    отладка

--- backend/api/submit_review.php
+++ backend/api/submit_review.php
@@ -1,5 +1,9 @@
 <?php
 // backend/api/submit_review.php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require __DIR__ . '/../functions.php';
 $config = require __DIR__ . '/../config.php';
 
