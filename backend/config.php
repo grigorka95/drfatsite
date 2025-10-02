f19cf6245df89aca07c7c1622f711cb8c14f5f77
grigorka95 <trane7775@gmail.com>
2025-10-02 11:14:56 +0300

    Update config.php
    конфиг под env файл

--- backend/config.php
+++ backend/config.php
@@ -1,13 +1,14 @@
 <?php
-
-// backend/config.php
-
+require __DIR__ . '/env_loader.php';
+loadEnv(__DIR__ . '/../.env');
 return [
-    'db_host' => 'sql110.infinityfree.com',
-    'db_name' => 'if0_40057416_reviews',
-    'db_user' => 'if0_40057416',
-    'db_pass' => 'm6z7qbmGpQ2b',
-    'admin_email' => 'dr.fatenko@example.com', // поставьте нужный email
-    'site_origin' => 'drfatenko.great-site.net', // для CORS / безопасности, замените на ваш домен (или '*')
+    'db_host' => $_ENV['DB_HOST'] ?? 'localhost',
+    'db_name' => $_ENV['DB_NAME'] ?? '',
+    'db_user' => $_ENV['DB_USER'] ?? '',
+    'db_pass' => $_ENV['DB_PASS'] ?? '',
+    'admin_email' => $_ENV['ADMIN_EMAIL'] ?? '',
+    'site_origin' => $_ENV['SITE_ORIGIN'] ?? '*',
     'max_reviews_per_ip_per_day' => 5,
+    'admin_user' => $_ENV['ADMIN_USER'] ?? 'admin',
+    'admin_pass' => $_ENV['ADMIN_PASS'] ?? 'changeme',
 ];
