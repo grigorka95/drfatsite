d11e8018d06932afad9d31a0d90a9b35d57de324
grigorka95 <trane7775@gmail.com>
2025-10-24 14:28:57 +0300

    Update get_token.php

--- backend/api/get_token.php
+++ backend/api/get_token.php
@@ -1,4 +1,6 @@
 <?php
 require __DIR__ . '/../functions.php';
+require __DIR__ . '/../session_init.php;
 header('Content-Type: application/json; charset=utf-8');
 echo json_encode(['token' => getCsrfToken()]);
+exit;
